3247f6cbc531ab14e5ddf7c7a39d20f3334299d2
john <john@edarmoc.com>
2014-06-15 22:47:00 -0400

    in td.sh removed exit status test for wget, in automated operation we do not want to stop if wget fails, at least because of network connectivity.  if the network is down we want to keep trying until it comes back up, right?  Although in future maybe we should not try constantly until it comes back up and there may be some kinds of wget failure that we should stop for...
    tested and get expected behavior.  on network down it keeps cycling, stops loop on control-c and continues loop on space bar from within fbi.

--- td.sh
+++ td.sh
@@ -11,40 +11,32 @@ while true
 do
 
 		# we have to rm first, otherwise the file might not get removed if loop was not completed on previous pass.
+		# -f tests for existence of file
 
 	if [ -f $FILE ]
 
-	then rm $FILE
+		then rm $FILE
 		
-	fi
-
+		fi
 
 
 	wget $URL$FILE
 
 
-
-		# $? contains the exit status of the last command which was executed
-		# -ne is a 'not equal' comparison
-
- 	if [ $? -ne 0 ]
-
-	then exit
-
 		# fbi is a utility to display an image with the framebuffer in a text terminal without using X windows
 		# -t 360 is the time to display each image (it can do slidshows of multiple images)
 		# -1 indicates how many times to loop through the slidshow.  we want just once around for our one image
 
-	else fbi -noverbose -t 360 -1 $FILE
-
-        fi
+	fbi -noverbose -t 360 -1 $FILE
 
 
+		# $? contains the exit status of the last command which was executed
+		# -ne is a 'not equal' comparison
 		# in this case $? refers to the return status of fbi, if we exited fbi via interupt then we want to stop the loop.
 
         if [ $? -ne 0 ]
 
-        then exit
+	        then exit
 
-	fi
+		fi
 done
