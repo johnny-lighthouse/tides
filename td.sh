8b918b4028b0fe29b63b1799378127a54e992bbd
john <john@edarmoc.com>
2014-06-15 22:36:16 -0400

    move file name and url to variables for simplicity and clarity.
    add comments to explain all elements I think I might not remember later.

--- td.sh
+++ td.sh
@@ -1,22 +1,46 @@
-while true;
+#!/bin/bash
+
+
+FILE=8545240_wl_24.png
+
+URL=http://tidesandcurrents.noaa.gov/ports/plots/
+
+
+while true
 
 do
 
-	if [ -f 8545240_wl_24.png ];
+		# we have to rm first, otherwise the file might not get removed if loop was not completed on previous pass.
 
-	then rm 8545240_wl_24.png;
+	if [ -f $FILE ]
+
+	then rm $FILE
 		
 	fi
 
-	wget http://tidesandcurrents.noaa.gov/ports/plots/8545240_wl_24.png
+
+
+	wget $URL$FILE
+
+
+
+		# $? contains the exit status of the last command which was executed
+		# -ne is a 'not equal' comparison
 
  	if [ $? -ne 0 ]
 
 	then exit
 
-	else fbi -noverbose -t 360 -1 8545240_wl_24.png
+		# fbi is a utility to display an image with the framebuffer in a text terminal without using X windows
+		# -t 360 is the time to display each image (it can do slidshows of multiple images)
+		# -1 indicates how many times to loop through the slidshow.  we want just once around for our one image
+
+	else fbi -noverbose -t 360 -1 $FILE
 
         fi
+
+
+		# in this case $? refers to the return status of fbi, if we exited fbi via interupt then we want to stop the loop.
 
         if [ $? -ne 0 ]
 
