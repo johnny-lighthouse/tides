0ea33292dc6c95b24342654d7c0456996abeff9f
john <john@edarmoc.com>
2014-07-16 19:03:02 -0400

    remove api_querry variable and replace with set_querry() function.
    set_querry() constructs a valid set of api parameters with default values and some input validation to deal with date vs begin/end_date parameter choice.

--- tides.py
+++ tides.py
@@ -1,24 +1,31 @@
 import requests
 
-api_querry = {
-    'date':'latest',
-    'station':'8545240',
-    'product':'water_level',
-    'datum':'mlw',
-    'units':'english',
-    'time_zone':'gmt',
-    'application':'phillyrd.org',
-    'format':'json'
-    }
+def set_querry(product,date,station='8545240',datum='mlw',units='english',tz='gmt',app='phillyrd.org',format='json'):
+    '''returns a dictionary suitable for calling api.
+    takes product type 'water_level' or 'predictions'
+    takes date of 'latest' or 'today' '''
+
+    if date == 'latest' or date == 'today':
+        return {
+        'date':date,
+        'station':station,
+        'product':product,
+        'datum':datum,
+        'units':units,
+        'time_zone':tz,
+        'application':app,
+        'format':format
+            }
 
 api_url = 'http://tidesandcurrents.noaa.gov/api/datagetter'  
 
-def querry_api():
-    data = requests.get(api_url,params=api_querry)
-    return data.json()
+def querry_api(params):
+    return requests.get(api_url,params=params).json()
 
 def unpack_latest(api_data):
     # latest data from noaa should be a dictionary with keys 'data' and 'metadata'.
     # data value is a list with one item which is another dictionary with unicode values.
     # for now we're only interested in t and v keys which are the timestamp and the tide height.
     return tuple([str( api_data[u'data'][0][u't']),float( api_data[u'data'][0][u'v'])])
+
+
