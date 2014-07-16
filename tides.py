c00234c726b235194486f59ee34a80e576cd5377
john <john@edarmoc.com>
2014-07-16 10:58:19 -0400

    generalize previous code.  modify url and requests.get() call to use requests' parameter functionality.  Also split my function into two parts to separate getting data from unpacking it.  I anticipate that i will have to modify the unpacking function to deal with historic data.

--- tides.py
+++ tides.py
@@ -1,10 +1,24 @@
 import requests
 
-url = 'http://tidesandcurrents.noaa.gov/api/datagetter?date=latest&station=8545240&product=water_level&datum=mlw&units=english&time_zone=gmt&application=phillyrd.org&format=json'
+api_querry = {
+    'date':'latest',
+    'station':'8545240',
+    'product':'water_level',
+    'datum':'mlw',
+    'units':'english',
+    'time_zone':'gmt',
+    'application':'phillyrd.org',
+    'format':'json'
+    }
 
-def get_data():
-    data = requests.get(url)
-    unpack = data.json()
-    #unpack is a dictionary with keys data and metadata. data value is a list with one index which is another dictionary with unicode values.
-    #we're only interested in t and v keys which are the timestamp and the tide height.
-    return tuple([str(unpack[u'data'][0][u't']),float(unpack[u'data'][0][u'v'])])
+api_url = 'http://tidesandcurrents.noaa.gov/api/datagetter'  
+
+def querry_api():
+    data = requests.get(api_url,params=api_querry)
+    return data.json()
+
+def unpack_latest(api_data):
+    # latest data from noaa should be a dictionary with keys 'data' and 'metadata'.
+    # data value is a list with one item which is another dictionary with unicode values.
+    # for now we're only interested in t and v keys which are the timestamp and the tide height.
+    return tuple([str( api_data[u'data'][0][u't']),float( api_data[u'data'][0][u'v'])])
